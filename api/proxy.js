994def1efef7372a5e4bbcb0af3b1083f9c35e58
Hj <75936962+Hou-jing@users.noreply.github.com>
2024-09-08 14:43:50 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://form.jotform.com/241986258790169";//your website url
+  let target = "https://en.wikipedia.org/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
